1294d9333101103c7bc2e12210cc16c5a801d844
Christina Zorenboehmer <81073205+Christina1281995@users.noreply.github.com>
2022-02-20 16:04:19 +0100

    Update 2022_02_20_15.py

--- src/2022_02_20_15.py
+++ src/2022_02_20_15.py
@@ -50,10 +50,10 @@ from requests_oauthlib import OAuth2Session
 
 # ---------------------------------------------- ENVS AND FUNCTIONS ----------------------------------------------------
 
+
 logger = logging.getLogger(__name__)
 auth_token_url = "https://auth.sen2cube.at/realms/sen2cube-at/protocol/openid-connect/token"
 auth_client_id = "iq-web-client"
-
 def fetch_token(username: str, password: str,
                 auth_token_url: str,
                 auth_client_id: str,
@@ -62,6 +62,7 @@ def fetch_token(username: str, password: str,
     Get OAuth token from auth_token_url and store in config_path_tokenfile
     :return: OAuth2Token with session information or None if any error
     """
+    
     try:
         client: Final[LegacyApplicationClient] = LegacyApplicationClient(client_id=auth_client_id)
         with OAuth2Session(client=client) as oauth_session:
@@ -83,7 +84,7 @@ def fetch_token(username: str, password: str,
     except Exception:
         arcpy.AddErrorMessage(f"Unknown error on authentication for token url {auth_token_url} as client {auth_client_id}.",
             exc_info=True, )
-
+        
 def refresh_token(token: OAuth2Token, auth_token_url: str, auth_client_id: str) -> OAuth2Token:
   try:
     client: Final[LegacyApplicationClient] = LegacyApplicationClient(client_id=auth_client_id)
@@ -99,7 +100,7 @@ def refresh_token(token: OAuth2Token, auth_token_url: str, auth_client_id: str) 
   except Exception:
     arcpy.AddError(f"Unknown error on authentication for token url {auth_token_url} as client {auth_client_id}.",
       exc_info=True,)
-
+    
 def get_points(aoi_in):
     """
     Takes the input extent in the provided spatial reference (most likely web mercator 3857 units: coordinates in meters)
@@ -107,22 +108,22 @@ def get_points(aoi_in):
     then reprojected into the required WGS84 projection system with decimal degrees.
     A new array of reprojected points is returned.
     """
-
+    
     # Get current environment and active map
     aprx = arcpy.mp.ArcGISProject("CURRENT")
     activeMap = aprx.activeMap
-
+    
     # Get Spatial Reference of Map (determines how the extent will be formatted)
     map_spatial_reference = activeMap.spatialReference.PCSCode
-
     # arcpy.AddMessage(f"Active Map Spatial Reference:  {str(activeMap.spatialReference.PCSCode)}")
+    
     # new empty arrays for points
     points = []
     reproj_points = []
-
+    
     # in standard web mercator # left, bottom, right, top, nan, nan, nan,nan
     individual_units = str(aoi_in).split()
-
+    
     # in standard web mercator left, bottom, right, top, nan, nan, nan,nan
     # p_lowleft
     points.append(arcpy.Point(float(individual_units[0]), float(individual_units[1])))
@@ -132,10 +133,10 @@ def get_points(aoi_in):
     points.append(arcpy.Point(float(individual_units[2]), float(individual_units[3])))
     # p_upperleft
     points.append(arcpy.Point(float(individual_units[0]), float(individual_units[3])))
-
+    
     # Specify current map spatial reference
     spatial_reference = arcpy.SpatialReference(map_spatial_reference)
-
+    
     # Reproject points and add to array "reproj_points"
     for point in points:
         # Create point geometry with points and given spatial reference
@@ -145,20 +146,21 @@ def get_points(aoi_in):
         reproj_points.append(projectedPointGeometry)
         #arcpy.AddMessage(" X:  {0}".format(projectedPointGeometry.firstPoint.X))
         #arcpy.AddMessage(" Y:  {0}".format(projectedPointGeometry.firstPoint.Y))
-
+        
     # return array of reprojected points
     return reproj_points
-
-
+  
+  
 # This is used to execute code if the file was run but not imported
 if __name__ == '__main__':
-
+  
     # -------------------------------------- STEP 1: USER LOGIN AND GET TOKEN ------------------------------------------
-
+    
+    
     token_text = ""
     username = ""
     arcpy.AddMessage(u"\u200B")
-
+    
     # create pop-up window for log in as soon as "Run" is clicked
     popup = tk.Tk()
     # style elements
@@ -167,27 +169,27 @@ if __name__ == '__main__':
     popup.geometry('400x150')
     popup.title('Sen2Cube User Login')
     popup.eval('tk::PlaceWindow . center')
-
+    
     # TODO get icon from web url
     # icon
     # raw_data = urlopen("https://github.com/Christina1281995/arcpy_toolbox_sen2cube/blob/main/src/Picture1.png").read()
     # image = ImageTk.PhotoImage(data=raw_data)
     # popup.tk.call('wm', 'iconphoto', popup._w, image)
-
+    
     # pop up window content
     L1 = tk.Label(popup, text="Username:", font=(14)).grid(row=0, column=0, padx=15, pady=15)
     L2 = tk.Label(popup, text="Password:", font=(14)).grid(row=1, column=0, padx=5, pady=5)
-
+    
     username_input = tk.StringVar()
     password_input = tk.StringVar()
-
+    
     # text entries (censor password entry)
     t1 = tk.Entry(popup, textvariable=username_input, font=(14)).grid(row=0, column=1)
     t2 = tk.Entry(popup, textvariable=password_input, font=(14), show='\u2022').grid(row=1, column=1)
-
+    
     login_time = ""
     successful_login = False
-
+    
     # Enter Key Function
     def enter(event):
         global username
@@ -199,14 +201,16 @@ if __name__ == '__main__':
         global login_time
         token_text = fetch_token(username, password, auth_token_url, auth_client_id)
         login_time = time.time()
-        arcpy.AddMessage(f"Login time: {str(login_time)}")
+        # arcpy.AddMessage(f"Login time: {str(login_time)}")
         successful_login = True
         arcpy.AddMessage('Login successful.')
         arcpy.AddMessage('_________________')
         arcpy.AddMessage(u"\u200B")
         popup.destroy()
+    
+    # Bind Enter key to the "enter()" function
     popup.bind('<Return>', enter)
-
+    
     # Login Button Function
     def login():
         global username
@@ -224,22 +228,22 @@ if __name__ == '__main__':
         arcpy.AddMessage('_________________')
         arcpy.AddMessage(u"\u200B")
         popup.destroy()
-
+        
     # Cancel Button Function
     def cancel():
         arcpy.AddError('Login process was cancelled.')
         popup.destroy()
         sys.exit(0)
-
+        
     b1 = tk.Button(popup, command=login, text='Login', font=(14)).grid(row=2, column=1, sticky=tk.W)
     b2 = tk.Button(popup, command=cancel, text='Cancel', font=(14)).grid(row=2, column=1, sticky=tk.E)
-
+    
     popup.mainloop()
-
-
+    
+    
     # ------------------------------------- STEP 2: SEND POST INFERENCE REQUEST ----------------------------------------
-
-
+    
+    
     # Inference Body Template
     # To be added dynamically: owner, AOI, temp_range_start, temp_range_end, comment, knowledgebase id, factbase id
     inference_body = {
@@ -270,23 +274,24 @@ if __name__ == '__main__':
             }
         }
     }
-
+    
     # TODO: implement checks on AOI (area size, location within given Factbase)
-
+    
     # Insert Area of Interest into Inference Body Text
     # The get_points() function defined above handles extent projection and point conversion
     aoi_points = get_points(arcpy.GetParameter(0))
+    
     # BBOX coordinate order: lower-left, lower-right, upper-right, upper-left, lower-left
     aoi = "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[" + \
           str(aoi_points[0].firstPoint.X) + "," + str(aoi_points[0].firstPoint.Y) + "],[" + str(aoi_points[1].firstPoint.X) + "," + str(aoi_points[1].firstPoint.Y) + "],[" + \
           str(aoi_points[2].firstPoint.X) + "," + str(aoi_points[2].firstPoint.Y) + "],[" + str(aoi_points[3].firstPoint.X) + "," + str(aoi_points[3].firstPoint.Y) + "],[" + \
                                                                                 str(aoi_points[0].firstPoint.X) + "," + str(aoi_points[0].firstPoint.Y) + "]]]}}]}"
-
+    
     inference_body["data"]["attributes"]["area_of_interest"] = aoi
-
+    
     # Insert username into inference body text
     inference_body["data"]["attributes"]["owner"] = str(username)
-
+    
     # Insert Factbase into Inference Body Text
     factbase_input = arcpy.GetParameterAsText(1)
     if factbase_input == "1 - Austria":
@@ -295,7 +300,7 @@ if __name__ == '__main__':
         inference_body["data"]["relationships"]["factbase"]["data"]["id"] = "2"
     elif factbase_input == "3 - Syria":
         inference_body["data"]["relationships"]["factbase"]["data"]["id"] = "3"
-
+        
     # Insert Knowledgebase into Inference Body Text
     knowledgebase_input = arcpy.GetParameterAsText(2)
     if knowledgebase_input == "00a - Hello World I":
@@ -310,33 +315,37 @@ if __name__ == '__main__':
         inference_body["data"]["relationships"]["knowledgebase"]["data"]["id"] = "2155"
     elif knowledgebase_input == "05 - SCBIR - Cloud free":
         inference_body["data"]["relationships"]["knowledgebase"]["data"]["id"] = "2153"
-
+        
     # TODO: Implement checks on time
+    
     # Insert Temp Ranges into Inference Body Text
     start = arcpy.GetParameter(3)
     end = arcpy.GetParameter(4)
-
+    
     # Check Start Date is before End Date
     if end < start:
         arcpy.AddError("The selected End Date is before the selected Start Date")
         sys.exit(0)
+        
     trs = start.strftime("%Y-%m-%dT00:00:00.000Z")
     tre = end.strftime("%Y-%m-%dT23:59:59.999Z")
+    
     inference_body["data"]["attributes"]["temp_range_start"] = str(trs)
     inference_body["data"]["attributes"]["temp_range_end"] = str(tre)
-
+    
     # Insert Comment into Inference Body Text
     comment_input = arcpy.GetParameterAsText(5)
     if comment_input != "":
         inference_body["data"]["attributes"]["comment"] = str(comment_input)
-
+        
     # Insert Favourite Preference into Inference Body Text
     favourite = arcpy.GetParameter(6)
     if favourite == True:
         inference_body["data"]["attributes"]["favourite"] = True
         # Default is False
-
+        
     # arcpy.AddMessage(inference_body)
+    
     # Assemble Post Request
     headers = {'Authorization': 'Bearer {}'.format(token_text["access_token"]), 'Content-Type': 'application/json'}
     url = "https://api.sen2cube.at/v1/inference"
@@ -358,34 +367,35 @@ if __name__ == '__main__':
     inference_endpoint = f"https://api.sen2cube.at/v1/inference/{str(inference_id)}"
     global status
     status = "-"
-
+    
     # Inform user
     arcpy.AddMessage(f"Inference ID: {str(inference_id)}")
     arcpy.AddMessage(u"\u200B")
     arcpy.AddMessage("The request is now being processed by Sen2Cube. This process may take up to a few minutes.")
     arcpy.AddMessage("Do not terminate the tool during this time, otherwise the inference will not be loaded into the map.")
     arcpy.AddMessage(u"\u200B")
-
+    
     # stop time
     t0 = time.time()
-
+    refresh = False
+    
     while status != "SUCCEEDED":
-
+        
+        # Check time to refresh token
         current_time = time.time()
         #arcpy.AddMessage(f"Current time: {str(current_time)}")
         elapsed_time = current_time - login_time
         #arcpy.AddMessage(f"Elapsed time: {str(elapsed_time)}")
-
-        if elapsed_time > 200:
+        if elapsed_time > 200 and refresh == False:
             # refresh
             token_text = refresh_token(token_text, auth_token_url, auth_client_id)
-            arcpy.AddMessage("200 seconds have elapsed since login - Token has been refreshed.")
-
+            arcpy.AddMessage("Token has been refreshed.")
+            refresh = True
+            
         # send request
         with requests.Session() as s:
             s.headers.update(headers)
             inference_current = s.get(inference_endpoint).json()
-
             # update status
             prev_status = status
             status = str(inference_current["data"]["attributes"]["status"])
@@ -397,75 +407,74 @@ if __name__ == '__main__':
                 # inform user
                 arcpy.AddError("Inference failed.")
                 sys.exit(0)
-
         # wait a little if not done yet
         if status != "SUCCEEDED":
             time.sleep(10)
-
+            
     global inference_complete
-
+    
     # Calculate time that it took to process inference
     t1 = time.time()
     diff = t1 - t0
     total = round(diff, 2)
-
+    
     # Inform user
     arcpy.AddMessage('_________________')
     arcpy.AddMessage(u"\u200B")
     arcpy.AddMessage(f'Inference {str(inference_id)}:')
     arcpy.AddMessage(f'Completed in {str(total)} seconds')
-
+    
+    # Get results
     with requests.Session() as s:
         s.headers.update(headers)
         inference_complete = s.get(inference_endpoint).json()
-
-    # TODO: Check how many outputs there are and handle
-
+        
     # Store some useful variables
     inference_output = json.loads(inference_complete["data"]["attributes"]["output"])
     output_url = []
     output_name = []
     output_type = []
-
+    
     # Get each of the outputs
     for output in inference_output:
         output_url.append(output["data"])
         output_name.append(output["name"])
         output_type.append(output["file_type"])
-
-
+        
+        
     # ---------------------------------- STEP 4: DOWNLOAD AND ADD INFERENCE TO MAP -------------------------------------
-
-
+    
+    
     # Inform user
     nr_of_outputs = len(output_name)
     arcpy.AddMessage(f"Number of outputs generated: {str(nr_of_outputs)}")
     arcpy.AddMessage(u"\u200B")
-
+    
     # Output directory
     out_dir = arcpy.GetParameterAsText(7)
-
+    
     # Current Python Project
     aprx = arcpy.mp.ArcGISProject('CURRENT')
     activeMap = aprx.activeMap
-
+    
     for i in range(nr_of_outputs):
         # User endpoint and data from inference to create download link
         download_link = f"https://demo.sen2cube.at{str(output_url[i])}"
-
+        
         # Get inference results
         with requests.Session() as s:
             s.headers.update(headers)
             outfile = s.get(download_link)
+            
             # Create file name according to output type and name
             if output_type[i] == "geotiff":
                 fname = f"{str(inference_id)}_{str(output_name[i])}.tiff"
             elif output_type[i] == "csv":
                 fname = f"{str(inference_id)}_{str(output_name[i])}.csv"
-
+                
             # Assemble complete path
             local_file = os.path.join(str(out_dir), fname)
-
+            
             # Check if file already exists
             if os.path.exists(local_file):
                 arcpy.AddMessage(
@@ -498,7 +507,7 @@ if __name__ == '__main__':
                 activeMap.addDataFromPath(local_file)
                 
         s.close()
-
+        
     # Inform user
     arcpy.AddMessage('_________________')
     arcpy.AddMessage(u"\u200B")
